ff1fe3b5781d43a2dfed908b670aecb33849b44e
Paul Barnetta <paul@barnetta.com>
2019-11-08 17:59:39 +1100

    Accept repository paths on the command-line
    We simply open and close the repos, but don't do anything with them yet.
    The main goal of this commit is to link against libgit2.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,31 @@
-int main() {
+#include <git2.h>
+#include <stdio.h>
+
+static int import_repository(const char *path) {
+
+	git_repository *repo = NULL;
+	int error = git_repository_open(&repo, path);
+	if (error) {
+		const git_error *e = git_error_last();
+		fprintf(stderr, "git-import-squares: %s\n", e->message);
+		return 1;
+	}
+
+	git_repository_free(repo);
+
 	return 0;
+}
+
+int main(int argc, char **argv) {
+
+	git_libgit2_init();
+
+	int result = 0;
+	for (int i = 1; i < argc && ! result; i++) {
+		result = import_repository(argv[1]);
+	}
+
+	git_libgit2_shutdown();
+
+	return result;
 }
