a1a7440833717825e8cda9bdf6c9dc22093f12a8
Paul Barnetta <paul@barnetta.com>
2019-11-08 19:49:53 +1100

    Open and close the destination repository

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <git2.h>
 #include <stdio.h>
 
+static struct {
+	git_repository *repo;
+} destination = { NULL };
+
 typedef int (*walk_func)(git_repository *repo, git_oid *oid);
 
 static int walk_repository(git_repository *repo, walk_func f) {
@@ -23,6 +27,23 @@ static int walk_repository(git_repository *repo, walk_func f) {
 	git_revwalk_free(walk);
 
 	return error;
+}
+
+static int open_repository(const char *path) {
+
+	int error = git_repository_open(&destination.repo, path);
+	if (error) {
+		const git_error *e = git_error_last();
+		fprintf(stderr, "git-import-squares: %s\n", e->message);
+		return 1;
+	}
+
+	return error;
+}
+
+static void close_repository() {
+
+	git_repository_free(destination.repo);
 }
 
 static int import_commit(git_repository *repo, git_oid *oid) {
@@ -61,12 +82,19 @@ int main(int argc, char **argv) {
 
 	git_libgit2_init();
 
-	int result = 0;
-	for (int i = 1; i < argc && ! result; i++) {
-		result = import_repository(argv[1]);
+	int error = open_repository(".");
+	if (error) {
+		git_libgit2_shutdown();
+		return 1;
 	}
+
+	for (int i = 1; ! error && i < argc; i++) {
+		error = import_repository(argv[1]);
+	}
+
+	close_repository();
 
 	git_libgit2_shutdown();
 
-	return result;
+	return error;
 }
