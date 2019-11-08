3bc6cd1244646957c27fb80f2510b309a8a6941a
Paul Barnetta <paul@barnetta.com>
2019-11-08 18:30:27 +1100

    Walk repository commits

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <git2.h>
 #include <stdio.h>
 
+static int import_commit(git_oid *oid) {
+
+	return 0;
+}
+
 static int import_repository(const char *path) {
 
 	git_repository *repo = NULL;
@@ -11,6 +16,20 @@ static int import_repository(const char *path) {
 		return 1;
 	}
 
+	git_revwalk *walk = NULL;
+	git_revwalk_new(&walk, repo);
+
+	error = git_revwalk_push_glob(walk, "*");
+	if (error) {
+		const git_error *e = git_error_last();
+		fprintf(stderr, "git-import-squares: %s\n", e->message);
+		return 1;
+	}
+
+	git_oid oid;
+	while (! git_revwalk_next(&oid, walk) && ! import_commit(&oid)) { }
+
+	git_revwalk_free(walk);
 	git_repository_free(repo);
 
 	return 0;
