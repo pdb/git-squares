65fa636ce3d761674c035a9220df85f00a77d29f
Paul Barnetta <paul@barnetta.com>
2019-11-08 22:44:18 +1100

    Begin adding new commits!

--- src/main.c
+++ src/main.c
@@ -150,6 +150,54 @@ static void close_repository() {
 static int import_commit(git_repository *repo, git_oid *oid,
 	git_commit *commit) {
 
+	/* Skip if this commit is already known to us */
+	for (struct commit *c = destination.commits; c; c = c->next) {
+		if (git_oid_equal(oid, &c->oid)) {
+			return 0;
+		}
+	}
+
+	char hash[GIT_OID_HEXSZ + 1];
+	snprintf(hash, sizeof hash, "%s", git_oid_tostr_s(oid));
+
+	git_commit *parent;
+	int error = git_commit_lookup(&parent, destination.repo,
+		&destination.head.oid);
+	if (error) {
+		const git_error *e = git_error_last();
+		fprintf(stderr, "git-import-squares: %s\n", e->message);
+		return 1;
+	}
+
+	destination.signature->when = (git_time) {
+		git_commit_time(commit),
+		git_commit_time_offset(commit),
+		git_commit_time_offset(commit) < 0 ? '-' : '+'
+	};
+
+	git_oid new_commit_oid;
+	error = git_commit_create_v(
+		&new_commit_oid,
+		destination.repo,
+		"HEAD",
+		destination.signature,
+		destination.signature,
+		"UTF-8",
+		hash,
+		destination.head.tree,
+		1,
+		parent
+	);
+	if (error) {
+		const git_error *e = git_error_last();
+		fprintf(stderr, "git-import-squares: %s\n", e->message);
+		return 1;
+	}
+
+	git_commit_free(parent);
+
+	git_oid_cpy(&destination.head.oid, &new_commit_oid);
+
 	return 0;
 }
 
