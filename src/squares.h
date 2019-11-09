6399800a253ea3eb358ea4da19763d9061aed9ae
Paul Barnetta <paul@barnetta.com>
2019-11-09 21:32:02 +1100

    Spin destination repo methods into their own file

--- src/squares.h
+++ src/squares.h
@@ -3,6 +3,28 @@
 
 #include <git2.h>
 
+struct commit {
+	git_oid oid;
+	git_time_t time;
+	struct commit *next;
+};
+
+struct destination {
+	git_repository *repo;
+	git_signature *signature;
+	struct {
+		git_oid oid;
+		git_tree *tree;
+	} head;
+	struct commit *commits;
+};
+
+extern struct destination destination;
+
+int register_commit(git_oid *oid, git_time_t time);
+int open_repository(const char *path);
+void close_repository();
+
 typedef int (*walk_func)(git_repository *repo, git_oid *oid, git_commit *commit);
 
 int walk_repository(git_repository *repo, walk_func f);
